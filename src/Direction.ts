4a3f4f60e95820af0c6ce98631fdff6a04450c57
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-06-22 21:04:05 +0530

    add Direction enums

---
+++ src/Direction.ts
@@ -0,0 +1,19 @@
+/**
+ * Enumeration to handle the move generation in n/e/s/w directions
+ */
+ export enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+/**
+ * Enumeration to handle the move generation in diagonal directions
+ */
+ export enum DirectionAngle {
+    UpRight,
+    UpLeft,
+    DownRight,
+    DownLeft
+}
